794f26d3746212c7df573f0d837c09c05ae7da50
4uvirik <chuvirik@yandex.ru>
2025-08-10 15:06:05 +0300

    fix: add error type

--- internal/handlers/proxy.go
+++ internal/handlers/proxy.go
@@ -17,6 +17,10 @@ type Handler struct {
 	Config *config.Config
 }
 
+type ErrorResponse struct {
+	Massage string `json:"massage"`
+}
+
 func (h *Handler) Proxy(c echo.Context) error {
 
 	ctx, cancel := context.WithTimeout(c.Request().Context(), time.Duration(h.Config.HTTPClient.Timeout)*time.Second)
@@ -25,19 +29,19 @@ func (h *Handler) Proxy(c echo.Context) error {
 	postIDStr := c.Param("id")
 	if postIDStr == "" {
 		h.Logger.Warn("no found ID in post")
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Post ID required"})
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Massage: "Post ID required"})
 	}
 
 	postID, err := strconv.Atoi(postIDStr)
 	if err != nil {
 		h.Logger.Warn("invalid ID format", "postID", postIDStr)
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Post ID must be number"})
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Massage: "Post ID must be number"})
 	}
 
 	post, err := h.Client.GetPost(ctx, postID)
 	if err != nil {
 		h.Logger.Error("failed to get post", "error", err)
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "failed to get post"})
+		return c.JSON(http.StatusInternalServerError, ErrorResponse{Massage: "failed to get post"})
 	}
 
 	return c.JSON(http.StatusOK, post)
