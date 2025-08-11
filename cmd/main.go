5a9614f26f210694c4acb03323f8cdb1b9ea4a1f
4uvirik <chuvirik@yandex.ru>
2025-08-11 13:33:34 +0300

    fix: remove configLogger from main.go in logger package

--- cmd/main.go
+++ cmd/main.go
@@ -4,24 +4,17 @@ import (
 	"github.com/4uvirik/ProxyForPublicAPI/config"
 	"github.com/4uvirik/ProxyForPublicAPI/internal/client"
 	"github.com/4uvirik/ProxyForPublicAPI/internal/handlers"
-	"github.com/4uvirik/ProxyForPublicAPI/internal/logger/handler/slogpretty"
+	"github.com/4uvirik/ProxyForPublicAPI/internal/logger"
 	"github.com/4uvirik/ProxyForPublicAPI/internal/server"
 	"log"
 	"log/slog"
-	"os"
-)
-
-const (
-	envLocal = "local"
-	envDev   = "dev"
-	envProd  = "prod"
 )
 
 func main() {
 
 	cfg := config.MustNew()
 
-	logger, err := configLogger(cfg.Logger.Level)
+	logger, err := logger.ConfigLogger(cfg.Logger.Level)
 	if err != nil {
 		log.Fatalf("error creating logger: %s", err.Error())
 	}
@@ -38,40 +31,4 @@ func main() {
 
 	server.Run(cfg, h, logger)
 
-}
-
-func configLogger(logLevel string) (*slog.Logger, error) {
-	var log *slog.Logger
-
-	switch logLevel {
-	case envLocal:
-		log = setupPrettySlog()
-	case envDev:
-		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug, AddSource: true}),
-		)
-	case envProd:
-		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-		)
-	default:
-		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-		)
-	}
-
-	return log, nil
-}
-
-func setupPrettySlog() *slog.Logger {
-	opts := slogpretty.PrettyHandlerOptions{
-		SlogOpts: &slog.HandlerOptions{
-			Level:     slog.LevelDebug,
-			AddSource: true,
-		},
-	}
-
-	handler := opts.NewPrettyHandler(os.Stdout)
-
-	return slog.New(handler)
 }
